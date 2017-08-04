305ad284301ac74b6d6753d7f30e6415c61f7151
David July <david.july1991@gmail.com>
2017-08-05 00:46:05 +1000

    Rename LmaReader to LmaDeserializer Apply FP to LmaDeserializer

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .handler import genWriter, Reader, Struct
 from .fmt import HitDeserializer, BinDeserializer
-from .lma import LmaReader
+from .lma import LmaDeserializer
 from .hist import Hist1D, Hist2D
 
 
 __all__ = ('genWriter', 'Reader', 'Struct',
-           'HitDeserializer', 'BinDeserializer', 'LmaReader',
+           'HitDeserializer', 'BinDeserializer', 'LmaDeserializer',
            'Hist1D', 'Hist2D')
--- lma.py
+++ lma.py
@@ -1,115 +1,96 @@
-from os.path import getsize
-from numpy import fromfile, append, nan, repeat, nan_to_num
+from numpy import fromfile, append, nan, repeat, nan_to_num, ndarray
 from struct import Struct
 from cytoolz import first, curry, pipe
 from cytoolz.curried import reduce, map
 from operator import add
 
 
-def read_bin(struct, buffer):
+@curry
+def read_bin(struct, buffer) -> iter:
     s = struct.size
     r = buffer.read(s)
     yield from struct.unpack(r)
 
 
-class LmaReader:
-    def __init__(self, filename):
-        self.__size = getsize(filename)
-        self.__file = open(filename, 'br')
-        self.__current_bit = 0
-        # short:h int32:i double:d unsighed int32:I
-        self.__header = tuple(read_bin(Struct("=ihhdidhdhIIh"), self.__file))
-
+class LmaDeserializer:
+    """
+    Deserializer for LMA format
+    Example:
+        from os.path import getsize
+        last = getsize(filename)
+        with open(filename, 'rb') as buffer:
+            deserialize = LmaDeserializer(buffer)
+            while buffer.tell() < last:
+                deserialized = deserialize(buffer)
+    """
+    def __init__(self, buffer):
+        """
+        Read header from buffer
+        :param buffer: binary buffer
+        """
+        self.__header = tuple(read_bin(Struct("=ihhdidhdhIIh"), buffer))
         channels = (
-            tuple(read_bin(Struct("=hhdhhii"), self.__file))
+            tuple(read_bin(Struct("=hhdhhii"), buffer))
             if ((self.used_channels >> i) & 0b1) == 1 else None
             for i in range(self.nchannels))
         self.__channels = tuple(
             {'fullscale': ch[0], 'gain': ch[2], 'baseline': ch[3]}
             if ch is not None else None
             for ch in channels)
-
-        self.__read_event = lambda: read_bin(Struct("=id"), self.__file)
-        self.__read_npulses = lambda: read_bin(Struct("=h"), self.__file)
-        self.__read_pulseinfo = lambda: read_bin(Struct("=ii"), self.__file)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__file.close()
+        self.__read_event = read_bin(Struct("=id"))
+        self.__read_npulses = read_bin(Struct("=h"))
+        self.__read_pulseinfo = read_bin(Struct("=ii"))
+        self.__read_pulse = curry(fromfile, dtype='int16')
 
     @property
-    def first_bit(self):
-        return self.header_size
-
-    @property
-    def last_bit(self):
-        return self.__size
-
-    @property
-    def __current_bit(self):
-        return self.__file.tell()
-
-    @__current_bit.setter
-    def __current_bit(self, i):
-        self.__file.seek(i)
-
-    @property
-    def current_bit(self):
-        return self.__current_bit
-
-    @property
-    def header(self):
+    def header(self) -> tuple:
         return self.__header
 
     @property
-    def header_size(self):
+    def header_size(self) -> int:
         return self.header[0] + 4
 
     @property
-    def nchannels(self):
+    def nchannels(self) -> int:
         return self.header[1]
 
     @property
-    def nbytes(self):
+    def nbytes(self) -> int:
         return self.header[2]
 
     @property
-    def sample_interval(self):
+    def sample_interval(self) -> float:
         return self.header[3]
 
     @property
-    def nsamples(self):
+    def nsamples(self) -> int:
         return self.header[4]
 
     @property
-    def used_channels(self):
+    def used_channels(self) -> int:
         return self.header[9]
 
     @property
-    def channels(self):
+    def channels(self) -> tuple(dict):
         return self.__channels
 
-    def __read_pulses(self):
+    def __read_pulses(self, buffer) -> iter(ndarray):
         n = self.nsamples
         nans = curry(repeat, nan)
-        for _ in range(first(self.__read_npulses())):
-            n0, n1 = self.__read_pulseinfo()
-            d = fromfile(self.__file, dtype='int16', count=n1)
+        for _ in range(first(self.__read_npulses(buffer))):
+            n0, n1 = self.__read_pulseinfo(buffer)
+            d = self.__read_pulse(buffer, count=n1)
             yield reduce(append, (nans(n0), d, nans(n-n0-n1)))
 
-    def __iter__(self):
-        self.__current_bit = self.first_bit
-        return self
-
-    def __next__(self):
-        if not (self.current_bit < self.last_bit):
-            raise StopIteration
-        return {'bit': self.current_bit,
-                'tag': first(self.__read_event()),
+    def __call__(self, buffer) -> dict:
+        """
+        Deserialize LMA file
+        :param buffer: binary buffer
+        :return: dict
+        """
+        return {'tag': first(self.__read_event(buffer)),
                 'channels': tuple(
-                    pipe(self.__read_pulses(),
+                    pipe(self.__read_pulses(buffer),
                          map(lambda pulse: (pulse-ch['baseline'])*ch['gain']),
                          map(nan_to_num),
                          reduce(add))
