8e242513d64be675b388cb3670905c498f5afcd3
David July <david.july1991@gmail.com>
2017-08-05 10:56:30 +1000

    Add document for LMA format

--- lma.py
+++ lma.py
@@ -31,8 +31,61 @@ class Deserializer(ABC):
 class LmaDeserializer(Deserializer):
     """
     Deserializer for LMA format
+    Data format:
+        1. Header part
+
+            general     channel
+            ––––––– n * –––––––
+
+            n is num of used channels
+
+            - general: length = 12
+                - (int32) + 4 = header size
+                - (int16) = num of channels
+                - (int16) = num of bytes
+                - (float64) = sample interval
+                - (int32) = num of samples
+                - round((float64), 100) = delay time
+                - (int16)
+                - (float64)
+                - (int16)
+                - (uint32) = used channels. If the value is 13, it is 0x1101 in binary, then only first, third, and
+                             fourth channels are used
+                - (uint32)
+                - (int16)
+            - channel: length = 7
+                - (int16) = full scale
+                - (int16) = offset
+                - (float64) = gain
+                - (int16) = baseline
+                - (int16)
+                - (int32)
+                - (int32)
+
+        2. Event part
+
+            event     num of partial pulses     info of a partial pulse  a partial pulse
+            ––––– n * ––––––––––––––––––––– m * –––––––––––––––––––––––  –––––––––––––––
+
+            n is num of used channels
+            m is num of partial pulses
+
+            - event: length = 2
+                - (int32) = tag
+                - (float64) = horpos
+            - num of partial pulses: length = 1
+                - (int16) num of partial pulses
+            - info of a partial pulse: length = 2
+                - (int32) = first index of a partial pulse
+                - (int32) = length of a partial pulse
+            - a partial pulse: length = length of a partial pulse
+                               It is equal with full pulse[first index:first index+length]. Final wave is
+                               gain*(full pulse - baseline).
+                - m * (int16)
+
     Example:
         from os.path import getsize
+
         last = getsize(filename)
         with open(filename, 'rb') as buffer:
             deserialize = LmaDeserializer(buffer)
@@ -59,32 +112,28 @@ class LmaDeserializer(Deserializer):
         self.__read_pulse = curry(fromfile, dtype='int16')
 
     @property
-    def header(self) -> tuple:
-        return self.__header
-
-    @property
     def header_size(self) -> int:
-        return self.header[0] + 4
+        return self.__header[0] + 4
 
     @property
     def nchannels(self) -> int:
-        return self.header[1]
+        return self.__header[1]
 
     @property
     def nbytes(self) -> int:
-        return self.header[2]
+        return self.__header[2]
 
     @property
     def sample_interval(self) -> float:
-        return self.header[3]
+        return self.__header[3]
 
     @property
     def nsamples(self) -> int:
-        return self.header[4]
+        return self.__header[4]
 
     @property
     def used_channels(self) -> int:
-        return self.header[9]
+        return self.__header[9]
 
     @property
     def channels(self) -> [dict]:
@@ -107,7 +156,7 @@ class LmaDeserializer(Deserializer):
         return {'tag': first(self.__read_event(buffer)),
                 'channels': tuple(
                     pipe(self.__read_pulses(buffer),
-                         map(lambda pulse: (pulse-ch['baseline'])*ch['gain']),
+                         map(lambda pulse: ch['gain']*(pulse-ch['baseline'])),
                          map(nan_to_num),
                          reduce(add))
                     if ch is not None else None for ch in self.channels)}
